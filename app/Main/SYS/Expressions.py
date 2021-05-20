29f87d58159bcc9bb578d8ab19d8dd53ac26a03c
Vedant K <82938580+dopevog@users.noreply.github.com>
2021-05-20 11:53:52 +0530

    ▶

---
+++ app/Main/SYS/Expressions.py
@@ -0,0 +1,83 @@
+from rply.token import BaseBox
+from Main.SYS.Types import IntType, StrType, BoolType, FloatType, List, NoneType
+import sys
+from Main.Errors import error, errors
+
+Types = {
+    "integer": IntType,
+    "string": StrType,
+    "boolean": BoolType,
+    "float": FloatType,
+    "list": List,
+    "none": NoneType
+}
+
+
+class ExpressionBase(BaseBox):
+    def __init__(self, value, kind, var=None):
+        self.value = value
+        if type(kind) == str:
+            self.kind = Types[kind](self)
+        else:
+            self.kind = kind
+        self.var = var
+
+    def eval(self):
+        if self.var is not None:
+            from Main.SYS.Variables import ListVar
+            if type(self.var) == ListVar:
+                self.var.eval()
+            self.value, self.kind = self.var.value, self.var.kind
+        return self.value
+
+    def gettype(self):
+        return self.kind
+
+    def sum(self, exp):
+        try:
+            self.value = self.kind.sum(self.eval(), exp.eval())
+        except:
+            self.value = exp.kind.sum(exp.eval(), self.eval())
+        return self.value
+
+    def sub(self, exp):
+        self.value = self.kind.sub(self.eval(), exp.eval())
+        return self.value
+
+    def increment(self):
+        self.value = self.kind.increment(self.eval())
+        return self.value
+
+
+class ExpressionFromList(ExpressionBase):
+    def __init__(self, var, indice):
+        self.value = ""
+        self.kind = Types["list"](self)
+        self.var = var
+        self.indice = indice
+
+    def eval(self):
+        var = self.var.exp.var
+        if len(var) <= self.indice:
+            values = self.var.exp.getexpression()
+            value = []
+            for i in values:
+                value.append(i.eval())
+            error(errors.INDEXOUTOFRANGE, "", {
+                "type": "max, index",
+                "index": self.indice,
+                "max": len(var)-1
+            })
+            sys.exit(1)
+        self.value, self.kind = var[self.indice].value, var[self.indice].kind
+        return self.value
+
+
+class Nothing(BaseBox):
+    @staticmethod
+    def eval():
+        return None
+
+    @staticmethod
+    def gettokentype():
+        return "statement"
---
+++ app/Main/SYS/UniqueOperators.py
@@ -0,0 +1,44 @@
+from rply.token import BaseBox
+import sys
+from Main.Errors import error, errors
+
+
+class UniqueOp(BaseBox):
+    def __init__(self, var):
+        self.var = var
+
+
+class Increment(UniqueOp):
+    def eval(self):
+        try:
+            self.var.value = self.var.value + 1
+            return self.var.value
+        except:
+            try:
+                self.var.value = self.var.expression().increment()
+                return self.var.value
+            except:
+                error(errors.IMPOSSIBLEOPERATION, "", {
+                    "type": "operationtype, var",
+                    "operationtype": "Increase",
+                    "var": self.var
+                })
+                sys.exit(1)
+
+
+class Decrement(UniqueOp):
+    def eval(self):
+        try:
+            self.var.value = self.var.value - 1
+            return self.var.value
+        except:
+            try:
+                self.var.value = self.var.expression().decrement()
+                return self.var.value
+            except:
+                error(errors.IMPOSSIBLEOPERATION, "", {
+                    "type": "operationtype, var",
+                    "operationtype": "Decrease",
+                    "var": self.var
+                })
+                sys.exit(1)
